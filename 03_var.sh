ceb749545c47f3a278476d97198cff8a42fedae6
diptendusre <diptendusre@gmail.com>
2026-05-09 00:15:56 +0530

    new
    comkit

--- 03_var.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-var_name=Diptendu
-echo $var_name
-readonly age=10
-echo "My Name is $var_name"
-echo "My Age is $age"
--- 04_array.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#Array
-myarray=(1 2 3 4 hello "hell")
-echo ${myarray[1]}
